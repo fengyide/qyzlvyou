2e992146e278c42643f3f06784e617176d98522f
fengyd <1070722821@qq.com>
2018-09-10 08:43:12 +0800

    第二次企业站

--- xuanxiangka.js
+++ xuanxiangka.js
@@ -1,41 +1,10 @@
 /*
 * @Author: 123
-* @Date:   2018-09-06 11:37:32
+* @Date:   2018-09-09 16:16:34
 * @Last Modified by:   123
-* @Last Modified time: 2018-09-10 08:39:02
+* @Last Modified time: 2018-09-09 16:17:59
 */
-
-
 window.onload=function(){
-
-	// let tops=document.querySelector("li");
-	// let bottoms=document.querySelectorAll(".yc");
-	// console.log(tops,bottoms);
-	// for(let i=0;i<bottoms.length;i++){
-	// 	tops.onmouseenter=function(){
-	// 		bottoms[i].style.display="block";
-	// 	}
-	// }
-	// 
-	// 
-	// //返回顶部
-	let backs=document.querySelector(".back");
-	console.log(backs);
-	
-	window.onscroll=function(){
-		let h=document.body.scrollTop || document.documentElement.scrollTop;
-		if(h>600){
-			backs.style.height="50px";
-		}
-		else if(h<600){
-			backs.style.height=0;
-		}
-		backs.onclick=function(){
-			animate(document.body,{scrollTop:0},600);
-			animate(document.documentElement,{scrollTop:0},600);
-		}
-	}
-
 	//选项卡标题移入移出
 	let jdbox=document.querySelectorAll(".jdbox");
 	let jdText=document.querySelectorAll(".jdText");
